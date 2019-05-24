ffb5e26db0780cf8e12d8fecc7576d909a0810d7
Yesbol Kulanbekov <esbolkz@yahoo.com>
2019-05-24 12:05:10 +0600

    add rootVC

---
+++ SharedCode/RootViewController.swift
@@ -0,0 +1,11 @@
+//
+//  RootViewController.swift
+//  SharedCode
+//
+//  Created by Yesbol Kulanbekov on 5/24/19.
+//  Copyright © 2019 Lobster. All rights reserved.
+//
+
+import Foundation
+
+//https://medium.com/@stasost/ios-root-controller-navigation-3625eedbbff
