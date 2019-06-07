02adf57b0b75161843e58f05864cbf1e3ea2200f
Yesbol Kulanbekov <esbolkz@yahoo.com>
2019-06-07 14:33:10 +0600

    add loading handler

---
+++ SharedCode/Protocols/LoadingHandler.swift
@@ -0,0 +1,74 @@
+//
+//  LoadingHandler.swift
+//  SharedCode
+//
+//  Created by Yesbol Kulanbekov on 6/7/19.
+//  Copyright © 2019 Lobster. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+public protocol LoadingHandler{
+    public func showLoading()
+    public func hideLoading()
+}
+
+public extension LoadingHandler where Self: UIViewController  {
+    
+    public func showLoading() {
+        let indicator = makeActivityIndicator()
+        view.addSubview(indicator)
+        
+        view.addSubview(makeDimmingView())
+        
+        NSLayoutConstraint.activate([
+            indicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            indicator.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            ])
+    }
+    
+    public func hideLoading() {
+        guard let indicator = view.viewWithTag(indicatorViewTag) else {
+            return
+        }
+        
+        indicator.removeFromSuperview()
+        
+        guard let dimmingView = view.viewWithTag(dimmingViewTag) else {
+            return
+        }
+        
+        dimmingView.removeFromSuperview()
+    }
+    
+    private func makeActivityIndicator() -> UIActivityIndicatorView {
+        if let indicator = view.viewWithTag(indicatorViewTag) as? UIActivityIndicatorView {
+            return indicator
+        }
+        
+        let indicator = UIActivityIndicatorView(style: .gray)
+        indicator.translatesAutoresizingMaskIntoConstraints = false
+        indicator.tag = indicatorViewTag
+        indicator.startAnimating()
+        return indicator
+    }
+    
+    private func makeDimmingView() -> UIView {
+        if let dimmingView = view.viewWithTag(dimmingViewTag)  {
+            return dimmingView
+        }
+        let dimmingView = UIView()
+        dimmingView.translatesAutoresizingMaskIntoConstraints = false
+        dimmingView.tag = dimmingViewTag
+        dimmingView.backgroundColor = UIColor(white: 0.0, alpha: 0.5)
+        dimmingView.frame = view.frame
+        return dimmingView
+    }
+}
+
+private let indicatorViewTag = 123456
+private let dimmingViewTag = 2131433242324
+
+
