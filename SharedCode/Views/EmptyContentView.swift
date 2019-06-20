d810534d79998bb70059d45b212a9db782322204
Yesbol Kulanbekov <esbolkz@yahoo.com>
2019-06-20 20:26:30 +0600

    add EmptyContentView

--- SharedCode/Views/EmptyContentView.swift
+++ SharedCode/Views/EmptyContentView.swift
@@ -1,98 +1,139 @@
-////
-////  EmptyContentView.swift
-////  MonsterTours
-////
-////  Created by Yesbol Kulanbekov on 7/1/18.
-////  Copyright © 2018 Monster Tours. All rights reserved.
-////
 //
-//import UIKit
+//  EmptyContentView.swift
+//  MonsterTours
 //
+//  Created by Yesbol Kulanbekov on 7/1/18.
+//  Copyright © 2018 Monster Tours. All rights reserved.
 //
-//import UIKit
-//
-//
-//class EmptyContentView: UIView {
-//    
-//    let subs = EmptyContentView.Subs()
-//
-//    override init(frame: CGRect) {
-//        super.init(frame: frame)
-//        setup()
-//        render(with: Props())
-//        layout(in: self)
-//    }
-//    
-//    required init?(coder: NSCoder) {
-//        super.init(coder: coder)
-//    }
-//}
-//
-///// Props configuration and init
-//
-//extension EmptyContentView {
-//    struct Props {
-//        let text: String
-//    }
-//}
-//
-//extension EmptyContentView.Props {
-//    init() {
-//        text = "You have no content!"
-//    }
-//}
-//
-///// View configuration, content and styling
-//
-//extension EmptyContentView: Renderable {
-//    struct Subs {
-//        let emtyContentWarningLabel = UILabel {
-//            $0.numberOfLines = 0
-//            $0.textAlignment = .center
-//            $0.font = UIFont.preferredFont(forTextStyle: .headline)
-//        }
-//    }
-//    
-//    func setup() {
-//        Layout.falseMask(self)
-//    }
-//    
-//    func render(with props: EmptyContentView.Props) {
-//        subs.emtyContentWarningLabel.text = props.text
-//    }
-//    
-//    func layout(in mainView: EmptyContentView) {
-//        EmptyContentViewLayout(mainView: mainView, subViews: subs).paint()
-//    }
-//    
-//}
-//
-///// View layout
-//
-//struct EmptyContentViewLayout: UIViewLayout {
-//    var mainView: EmptyContentView
-//    var subViews: EmptyContentView.Subs
-//    
-//    func addSubViews() {
-//        mainView.addSubview(subViews.emtyContentWarningLabel)
-//    }
-//    
-//    func addConstraints() {
-//        subViews.emtyContentWarningLabel.anchor(top: mainView.topAnchor,
-//                                                leading: mainView.leadingAnchor,
-//                                                trailing: mainView.trailingAnchor,
-//                                                padding: UIEdgeInsets(top: 70, left: 30, bottom: 0, right: 30))
-//    }
-//
-//    
-//}
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
+
+import UIKit
+
+
+
+public class EmptyContentView: UIView {
+    
+    public struct Props {
+        public let title: String
+        public let subtitle: String
+        
+        public init(title: String, subtitle: String) {
+            self.title = title
+            self.subtitle = subtitle
+        }
+        
+        static let zero = Props(
+            title: "Main Text", subtitle: "Subtitle"
+        )
+    }
+    
+    struct Subviews {
+        
+        let icon = UIImageView()
+        let title = UILabel()
+        let subtitle = UILabel()
+        
+        func render(with props: Props) {
+            title.text = props.title
+            subtitle.text = props.subtitle
+        }
+        
+        func renderConstantData() {
+            icon.image = UIImage(named: "emptyBasket")
+            title.text = "Main Text"
+            subtitle.text = "Subtitle"
+        }
+    }
+    
+    /// Properties
+    
+    let sv = Subviews()
+    
+    public var props = Props.zero {
+        didSet {
+            sv.render(with: props)
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupSelf()
+        configureSubViews()
+        styleSubviews()
+        sv.render(with: Props.zero)
+        sv.renderConstantData()
+        EmptyContentViewLayout(for: self).paint()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
+
+/// View configuration, content and styling
+
+extension EmptyContentView {
+    
+    private func styleSubviews() {
+        sv.title.font = .preferredFont(forTextStyle: .largeTitle)
+        sv.subtitle.font = .preferredFont(forTextStyle: .subheadline)
+    }
+    
+    private func configureSubViews() {
+        
+    }
+    
+    private func setupSelf() {
+        
+    }
+    
+}
+
+
+
+/// View layout
+
+struct EmptyContentViewLayout {
+    
+    var rootView: EmptyContentView
+    var sv: EmptyContentView.Subviews
+    let rootStack = UIStackView(Stack.vStack, Stack.padStack)
+    
+    init(for rootView: EmptyContentView) {
+        self.rootView = rootView
+        self.sv = rootView.sv
+    }
+    
+    func paint() {
+        addSubViews()
+        addConstraints()
+    }
+}
+
+extension EmptyContentViewLayout {
+    
+    func addSubViews() {
+        rootView.addSubview(sv.icon)
+        rootView.addSubview(sv.title)
+        rootView.addSubview(sv.subtitle)
+    }
+    
+    func addConstraints() {
+        
+        sv.icon.addConstraints(
+            equal(rootView, Ancs.top, constant: 200),
+            equal(rootView, Ancs.verticalCenter)
+        )
+
+        sv.title.addConstraints(
+            equal(sv.icon, Ancs.top, Ancs.bottom, constant: 30),
+            equal(rootView, Ancs.verticalCenter)
+        )
+        
+        sv.subtitle.addConstraints(
+            equal(sv.title, Ancs.top, Ancs.bottom, constant: 10),
+            equal(rootView, Ancs.verticalCenter)
+        )
+        
+    }
+    
+}
